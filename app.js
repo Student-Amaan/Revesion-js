f618fb44ca84297a1c5f685ae243aba564307eff
waxir <danishdarman06@gmail.com>
2026-02-25 14:22:40 +0500

    revesion of let, const, and var

---
+++ app.js
@@ -0,0 +1,38 @@
+// Variables & Data Types (45 min)
+// Topics:
+
+// let const var (difference samjho)
+
+// String Number Boolean
+
+// Array Object
+
+// typeof
+
+// let ko hum change our update kar saktha ha e.g=>
+
+let num = 22;
+
+// console.log(num)
+num = 12;
+
+num = 33;
+console.log(num);
+
+// let sirf block scope ka andar kam kartha ha e.g =>
+
+if (true) {
+  let name = "dnaish";
+  console.log(name);
+}
+// let ko dobara re-declear nahi karsakhta
+
+// let a = 'alpha' ghalt ho gata ha
+// let a = 'Beta'
+
+// correct method
+
+let a = "alpha";
+a = "Beta";
+
+console.log(a)
